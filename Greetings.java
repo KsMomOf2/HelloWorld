2c0a9e1f8b3b16f7ffb816b40f4621db3cb3186e
Terri Kelly <kellyt@bishopireton.org>
2020-09-01 10:13:43 -0400

    Provide Code for Lab 1

---
+++ Greetings.java
@@ -0,0 +1,16 @@
+/* Name: 
+   Date:
+   Description: 
+*/
+
+public class Greetings {
+
+   public static void main(String[] args) {
+
+      String firstName = args[0];
+      String lastName = args[1];
+
+      System.out.println("Hello, " + firstName + " " + lastName);
+      System.out.println("Congratulations on your second program");
+   }
+}
